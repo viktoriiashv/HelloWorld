27adc250fc78c1c03d8a721beec2136d135d9b8e
viktoriiashv <viktoriiash@tahometer.com>
2020-08-03 17:41:33 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hello_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String message = BuildMessage("World");
+            Console.WriteLine(message);
+        }
+
+        static String BuildMessage(String name)
+        {
+            return "Hello " + name + "!";
+        }
+    }
+}
