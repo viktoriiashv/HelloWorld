e77178abbbe0eddb5d183e41f8d8994ca679b053
viktoriiashv <viktoriiash@tahometer.com>
2020-08-04 11:22:10 +0300

    3-arguments function ready

--- Program.cs
+++ Program.cs
@@ -4,15 +4,36 @@ namespace hello_console
 {
     class Program
     {
+        public struct DateRange
+        {
+            DateTime Start { get; set; }
+            DateTime End { get; set; }
+        
+        }
         static void Main(string[] args)
         {
-            String message = BuildMessage("World");
-            Console.WriteLine(message);
+            DateTime [] holidays  = {new DateTime(2020, 12, 31), new DateTime(2021, 1, 1)};
+            int count = CountWorkingDays(new DateTime(2020, 12, 29), new DateTime(2021, 1, 2), holidays);
+            Console.WriteLine(count);
         }
 
-        static String BuildMessage(String name)
+        public static int CountWorkingDays (DateTime start, DateTime end, DateTime[] holidays)
         {
-            return "Hello " + name + "!";
+            int countWorkingDays = 0;
+            while (start <= end){
+               DateTime d = start.AddDays(1);
+               //Console.WriteLine(start.AddDays(1));
+                if(!Array.Exists(holidays, element => element == d)){
+                    countWorkingDays++;
+                }
+                start = d;
+            }
+            return countWorkingDays;
+        }
+
+         public static int CountWorkingDays (DateRange range, DateTime[] holidays)
+        {
+            return 0;
         }
     }
 }
